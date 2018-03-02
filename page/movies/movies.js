881e1f407ba1b4267bbcf11b31fccb65fbb42d2a
lxb365 <36372674+lxb365@users.noreply.github.com>
2018-03-02 17:47:41 +0800

    03

--- page/movies/movies.js
+++ page/movies/movies.js
@@ -4,7 +4,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    urlpath: [
+      'http://t.yushu.im/v2/movie/in_theaters',
+      'http://t.yushu.im/v2/movie/coming_soon',
+      'http://t.yushu.im/v2/movie/top250'
+    ],
+    movieData: [
+      {
+        catogray:null,
+        movieList:[{},{},{}]
+      },
+      {
+        catogray: null,
+        movieList: [{}, {}, {}]
+      },
+      {
+        catogray: null,
+        movieList: [{}, {}, {}]
+      }
+    ]
   },
 
   /**
@@ -12,27 +30,53 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
-    wx.request({
-      url: 'http://t.yushu.im/v2/movie/in_theaters',
-      success:function(event){
-        console.log(event)
-      }
-    });
-
-    wx.request({
-      url: 'http://t.yushu.im/v2/movie/coming_soon',
-      success: function (event) {
-        console.log(event)
-      }
-    });
-
-    wx.request({
-      url: 'http://t.yushu.im/v2/movie/top250',
-      success: function (event) {
-        console.log(event)
-      }
-    })
+    var urlPath = this.data.urlpath;
+    for (let idx in urlPath) {
+      wx.request({
+        url: urlPath[idx] + "?start=0&count=3",
+        success: function (event) {      
+          that.saveData(event.data.subjects, event.data.total);
+        }
+      });
+    }
   },
 
-  
+  saveData: function (subjects,total) {
+    var tempMovieData = [{},{},{}];   
+
+    for(let i = 0;i < 3; i++){
+      var tempTitle = subjects[i].title;
+      if (tempTitle.length > 6)
+      {
+        tempTitle = tempTitle.substring(0,6)+"...";
+      }
+      tempMovieData[i] = {
+        title: tempTitle,
+        images: subjects[i].images.large,
+        rating: subjects[i].rating.average,
+        stars: subjects[i].rating.stars
+      }
+    }
+    var tempMovieList = this.data.movieData;
+    switch(total){
+      case 250:
+        tempMovieList[2].catogray = "Top250"
+        tempMovieList[2].movieList = tempMovieData;
+        break;
+      case 23:
+        tempMovieList[0].catogray = "正在热映"
+        tempMovieList[0].movieList = tempMovieData;
+        break;
+      case 26:
+        tempMovieList[1].catogray = "即将上映"
+        tempMovieList[1].movieList = tempMovieData;
+        break;
+    }
+    console.log(tempMovieList);
+    
+    this.setData({
+      movieData: tempMovieList
+    })
+    console.log(this.data.movieData);
+  }
 })
